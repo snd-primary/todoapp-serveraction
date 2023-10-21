f7b5778107dc174eb2436efb876b7d66171c4d83
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-21 05:49:13 +0000

    installed  shadcn-ui > card,input,label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { CardWithForm } from "./components/CardWithForm";
 
 export default function Home() {
   return (
     <main>
       <Button>ぼたんです</Button>
+      <CardWithForm />
     </main>
   );
 }
