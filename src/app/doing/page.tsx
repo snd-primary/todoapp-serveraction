fc8d20f28e4e7ac75b820124dad0aebd7cac62b1
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-26 13:49:04 +0000

    framer motion 導入

--- src/app/doing/page.tsx
+++ src/app/doing/page.tsx
@@ -4,6 +4,81 @@ import { useTodo } from "@/context/TodoProvider";
 import { totalSeconds } from "@/lib/totalSeconds";
 import { useEffect, useRef } from "react";
 
+import { motion } from "framer-motion";
+
+const ProgressCircle = ({}) => {
+  const size: number = 100;
+  const radius = 45;
+  const strokeWidth = 6;
+  const emptyStrokeOpacity = 0.25;
+  const circumference = Math.ceil(2 * Math.PI * radius);
+  const fillPercents = Math.abs(Math.ceil((circumference / 100) * (100 - 100)));
+  const transition = {
+    duration: 3,
+    delay: 0.5,
+    ease: "easeIn",
+  };
+
+  const variants = {
+    hidden: {
+      strokeDashoffset: circumference,
+      transition,
+    },
+    show: {
+      strokeDashoffset: fillPercents,
+      transition,
+    },
+  };
+
+  return (
+    <>
+      <svg
+        viewBox="0 0 100 100"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+      >
+        <circle
+          cx="50"
+          cy="50"
+          r={radius}
+          className="circle"
+          strokeWidth={strokeWidth}
+          stroke="#f0f0f0"
+          strokeOpacity={emptyStrokeOpacity}
+          fill="transparent"
+        />
+      </svg>
+      <svg
+        viewBox="0 0 100 100"
+        width={size}
+        height={size}
+        style={{
+          position: "absolute",
+          transform: "rotate(-90deg)",
+          overflow: "visible",
+          marginLeft: -size,
+        }}
+      >
+        <motion.circle
+          cx="50"
+          cy="50"
+          r={radius}
+          strokeWidth={strokeWidth}
+          stroke="#333"
+          fill="transparent"
+          strokeDashoffset={fillPercents}
+          strokeDasharray={circumference}
+          variants={variants}
+          initial="hidden"
+          animate={"show"}
+        />
+      </svg>
+    </>
+  );
+};
+
 const TodoItem: React.FC = () => {
   const [todo] = useTodo();
   const TOTAL_SECONDS = totalSeconds(todo);
@@ -13,22 +88,7 @@ const TodoItem: React.FC = () => {
     <>
       <div>
         <h2 className="text-4xl font-bold">{todo?.title}</h2>
-        <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M64 1 A63 63 0 1 1 64 127 A63 63 0 1 1 64 1"
-            stroke="#ccc"
-            stroke-width="2"
-            fill="none"
-          />
-        </svg>
-        <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M64 1 A63 63 0 1 1 64 127 A63 63 0 1 1 64 1"
-            stroke="#333"
-            stroke-width="2"
-            fill="none"
-          />
-        </svg>
+        <ProgressCircle />
         <CountDown initialCount={TOTAL_SECONDS} />
       </div>
     </>
