99abe4dca12f94728462496c3288b767669b4bf2
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-27 08:27:36 +0000

    progress circle 基本形実装done

--- src/app/doing/page.tsx
+++ src/app/doing/page.tsx
@@ -2,93 +2,15 @@
 import CountDown from "@/components/CountDown";
 import { useTodo } from "@/context/TodoProvider";
 import { totalSeconds } from "@/lib/totalSeconds";
-import { useEffect, useRef } from "react";
-
-import { motion } from "framer-motion";
-
-const ProgressCircle = ({}) => {
-  const size: number = 100;
-  const radius = 45;
-  const strokeWidth = 6;
-  const emptyStrokeOpacity = 0.25;
-  const circumference = Math.ceil(2 * Math.PI * radius);
-  const fillPercents = Math.abs(Math.ceil((circumference / 100) * (100 - 100)));
-  const transition = {
-    duration: 3,
-    delay: 0.5,
-    ease: "easeIn",
-  };
-
-  const variants = {
-    hidden: {
-      strokeDashoffset: circumference,
-      transition,
-    },
-    show: {
-      strokeDashoffset: fillPercents,
-      transition,
-    },
-  };
-
-  return (
-    <>
-      <svg
-        viewBox="0 0 100 100"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-      >
-        <circle
-          cx="50"
-          cy="50"
-          r={radius}
-          className="circle"
-          strokeWidth={strokeWidth}
-          stroke="#f0f0f0"
-          strokeOpacity={emptyStrokeOpacity}
-          fill="transparent"
-        />
-      </svg>
-      <svg
-        viewBox="0 0 100 100"
-        width={size}
-        height={size}
-        style={{
-          position: "absolute",
-          transform: "rotate(-90deg)",
-          overflow: "visible",
-          marginLeft: -size,
-        }}
-      >
-        <motion.circle
-          cx="50"
-          cy="50"
-          r={radius}
-          strokeWidth={strokeWidth}
-          stroke="#333"
-          fill="transparent"
-          strokeDashoffset={fillPercents}
-          strokeDasharray={circumference}
-          variants={variants}
-          initial="hidden"
-          animate={"show"}
-        />
-      </svg>
-    </>
-  );
-};
 
 const TodoItem: React.FC = () => {
   const [todo] = useTodo();
   const TOTAL_SECONDS = totalSeconds(todo);
 
-  const pathRef = useRef();
   return (
     <>
       <div>
         <h2 className="text-4xl font-bold">{todo?.title}</h2>
-        <ProgressCircle />
         <CountDown initialCount={TOTAL_SECONDS} />
       </div>
     </>
--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,6 +1,54 @@
+import { formatTime } from "@/lib/formatTime";
+import { Scale } from "lucide-react";
 import React, { useEffect, useRef, useState } from "react";
 type Props = {
   initialCount: number;
+};
+
+interface CirclarProgressProps {
+  size: number;
+  strokeWidth: number;
+  count: number;
+  initialCount: number;
+  color: string;
+}
+
+const CirclarProgress: React.FC<CirclarProgressProps> = ({
+  size,
+  strokeWidth,
+  count,
+  color,
+  initialCount,
+}) => {
+  const viewBox = `0 0 ${size} ${size}`;
+  const radius = (size - strokeWidth) / 2;
+  // 円周
+  const circrmference = radius * Math.PI * 2;
+
+  // 破線の長さ
+  const dash = ((initialCount - count) * circrmference) / initialCount;
+  const offset = ((initialCount - count) / initialCount) * circrmference;
+  console.log(dash);
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox={viewBox}
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: "scale(-1, 1)" }}
+    >
+      <circle
+        fill="none"
+        stroke={color}
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        transform={`rotate(-90 ${size / 2} ${size / 2}) `}
+        strokeWidth={`${strokeWidth}`}
+        strokeDasharray={`${circrmference - dash} ${dash}`}
+      />
+    </svg>
+  );
 };
 
 const CountDown: React.FC<Props> = ({ initialCount }) => {
@@ -33,21 +81,18 @@ const CountDown: React.FC<Props> = ({ initialCount }) => {
     };
   }, [count, isRunning]);
 
-  const formatTime = (seconds: number) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-
-    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
-      2,
-      "0"
-    )}:${String(remainingSeconds).padStart(2, "0")}`;
-  };
-
   return (
     <div>
       <p>残り時間：{formatTime(count)}</p>
-      <div className="progress-circle"></div>
+      <div className="progress-circle">
+        <CirclarProgress
+          initialCount={initialCount}
+          size={250}
+          strokeWidth={20}
+          count={count}
+          color="green"
+        />
+      </div>
     </div>
   );
 };
